1e44f217283c1056f3fcbb1b384d0eb5b2917004
ErlendsZ <73559679+ErlendsZ@users.noreply.github.com>
2021-07-01 16:57:40 +0300

    Add files via upload

---
+++ junior_test/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Junior_Test </title>
+	<link rel="stylesheet" href="Text.css" />
+	<link rel="stylesheet" href="Containers.css" />
+	<link rel="stylesheet" href="Social.css" />
+	<link rel="stylesheet" href="Heading.css" />
+	<link rel="stylesheet" href="Terms.css" />
+	<link rel="stylesheet" href="Input.css" />
+</head>
+<body>
+<style>
+
+</style>
+<form action="http://localhost/j_test/index.php#">
+	<input class="email_input" id="email_input" type="text" name="email" 
+		placeholder="Type your email adress here..." >
+		<input class="Arrow" value="" type="submit">
+</form> 
+<span class="About_txt">
+<a href="#" style="text-decoration:none;">About</a>
+</span>
+
+<span class="How_works">
+<a href="#" style="text-decoration:none;">How it works</a>
+</span>
+
+<div class="Contact">
+<a href="#" style="text-decoration:none;">Contact</a>
+</div>
+
+	<div class="container">
+		<div class="TOS">
+
+				<label class="container_checkbox">
+					<input type="checkbox" checked="checked">
+					<span class="checkmark"></span>
+				</label>
+
+			<div class="TOS_txt">
+				I agree to  <a href="#" >terms of service</a>
+			</div>
+		</div>
+				
+		<div class="Input">
+
+				<div class="Line">
+				
+				</div>
+			</div>
+			
+		</div>
+
+		<div class="grid_news">
+			<h1> Subscribe to newsletter </h1>
+			<h3> Subscribe to our newsletter and get 10% discount on pineapple glasses. </h3>
+		</div>
+
+	<div class="Top_bar">
+		<div class="Pineapple_container">
+			<div class="Pineapple_logo">
+				<img src="Images/logo_pineapple.svg" alt="Pineapple logo">
+			</div>
+			
+		</div>
+	</div>
+
+		<div class="Social_Icons">
+
+			<div class="Facebook">
+				<div class="FB_base">
+					<img src="Images/Base.svg" alt="Facebook base">
+					<div class="FB_icon">
+						<img src="Images/ic_facebook.svg" alt="Facebook icon">
+					</div>
+				</div>
+			
+			<div class="Instagram">
+				<div class="IG_base">
+					<img src="Images/Base.svg" alt="Instagram base">
+					<div class="IG_icon">
+						<img src="Images/ic_instagram.svg" alt="Instagram icon">
+					</div>
+				</div>
+			</div>
+
+			<div class="Twitter">
+				<div class="TW_base">
+					<img src="Images/Base.svg" alt="Twitter base">
+					<div class="TW_icon">
+						<img src="Images/ic_twitter.svg" alt="Twitter icon">
+					</div>
+				</div>
+			</div>
+
+			<a href="#">
+			<div class="YouTube">
+					<div class="YT_base">
+							<div class="YT_icon">
+							
+							</div>
+						
+					</div>
+					
+			</div>
+			</a>
+		</div>
+	</div>
+
+	
+	<div class="grid_pineapple">
+		<img src="Images/Summer.jpg" alt="Summper Pineapples">	
+	</div>
+		
+	</div>
+	
+
+</body>
+</html> 
