6c82b131fb8eb004d6bff9e74a8f0c4e533ebdb6
ErlendsZ <73559679+ErlendsZ@users.noreply.github.com>
2021-07-03 15:21:26 +0300

    Add files via upload

---
+++ junior_test/success.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Junior_Test </title>
+	<link rel="stylesheet" href="Styles/Text_success_page.css" />
+	<link rel="stylesheet" href="Styles/Containers_success_page.css" />
+	<link rel="stylesheet" href="Styles/Social_success_page.css" />
+	<link rel="stylesheet" href="Styles/Heading.css" />
+	<link rel="stylesheet" href="Styles/Terms.css" />
+	<link rel="stylesheet" href="Styles/Input.css" />
+	<script defer src="js/email_validations.js"></script>
+</head>
+<body>
+
+
+
+<div id="error"></div>
+
+<span class="About_txt">
+<a href="#" style="text-decoration:none;">About</a>
+</span>
+
+<span class="How_works">
+<a href="#" style="text-decoration:none;">How it works</a>
+</span>
+
+<div class="Contact">
+<a href="#" style="text-decoration:none;">Contact</a>
+</div>
+	
+			<div class="Success">
+				<img src="Images/ic_success.svg" alt="Succes cup">	
+			</div>
+		</div>
+			<h1> Thanks for subscribing! </h1>
+			<h3> You habe successfully subscribed to our email listing. Check your email fo the discount code</h3>	
+	<div class="Top_bar">
+		<div class="Pineapple_container">
+			<div class="Pineapple_logo">
+				<img src="Images/logo_pineapple.svg" alt="Pineapple logo">
+			</div>
+			
+		</div>
+	</div>
+
+		<div class="Social_icon_line">
+
+		</div>
+
+		<div class="Social_Icons">
+	
+			<a href="#">
+			<div class="Facebook">
+				<div class="FB_base">
+					<div class="FB_icon">
+					</div>
+				</div>
+			</div>
+			</a>
+
+			<a href="#">
+			<div class="Instagram">
+				<div class="IG_base">
+					<div class="IG_icon">
+					</div>
+				</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="Twitter">
+				<div class="TW_base">
+					<div class="TW_icon">
+					</div>
+				</div>
+			</div>
+			</a>
+
+			<a href="#">
+			<div class="YouTube">
+					<div class="YT_base">
+						<div class="YT_icon">
+
+						</div>
+					</div>
+			</div>
+			</a>
+		</div>
+	</div>
+
+	
+	<div class="grid_pineapple">
+		<img src="Images/Summer.jpg" alt="Summper Pineapples">	
+	</div>
+		
+	</div>
+	
+
+</body>
+</html> 
